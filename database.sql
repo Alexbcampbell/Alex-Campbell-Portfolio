0db2bd2b4d274658173e603a25500b8c2c5b3512
Alex Campbell <alexbe.campbell@gmail.com>
2021-02-11 11:36:12 -0600

    added amazon s3 details to env and updated database.sql with new table

--- database.sql
+++ database.sql
@@ -3,8 +3,10 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE "contact" (
+firstName VARCHAR(50),
+lastName VARCHAR(50),
+phoneNumber VARCHAR(50),
+email VARCHAR(50),
+comments VARCHAR(500)
 );
