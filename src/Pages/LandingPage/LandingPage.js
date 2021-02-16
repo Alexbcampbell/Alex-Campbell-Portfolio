3e2a87b12e6e36323989218f18c0ca0d7e91234a
Alex Campbell <alexbe.campbell@gmail.com>
2021-02-15 22:59:56 -0600

    playing with CSS grid outline

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -6,7 +6,13 @@ import './LandingPage.css';
 
 class LandingPage extends Component {
   render() {
-    return <div className="container"></div>;
+    return (
+      <div className="container">
+        <div class="item">1</div>
+        <div class="item">2</div>
+        <div class="item">3</div>
+      </div>
+    );
   }
 }
 
