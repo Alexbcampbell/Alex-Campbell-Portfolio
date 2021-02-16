441010f591682e9739d44a949bfd148f16becfa4
Alex Campbell <alexbe.campbell@gmail.com>
2021-02-15 23:11:39 -0600

    changed styling, added background photo and profile photo for test, nav bar changed to transparent

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 import './LandingPage.css';
+const alexPic = require('./alex1_bw.png');
 
 class LandingPage extends Component {
   render() {
     return (
       <div className="container">
-        <div class="item">1</div>
+        <div class="item">
+          <img class="profile" src={alexPic} alt="black and white" />
+        </div>
         <div class="item">2</div>
         <div class="item">3</div>
       </div>
