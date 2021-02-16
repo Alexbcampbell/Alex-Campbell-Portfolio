e09126b358fca71c19deba8a3e15795b1cd9ea75
Alex Campbell <alexbe.campbell@gmail.com>
2021-02-16 14:59:03 -0600

    added portfolio reducer

---
+++ src/redux/reducers/portfolio.reducer.js
@@ -0,0 +1,12 @@
+const portfolioReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PROJECT':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// user will be on the redux state at:
+// state.user
+export default portfolioReducer;
