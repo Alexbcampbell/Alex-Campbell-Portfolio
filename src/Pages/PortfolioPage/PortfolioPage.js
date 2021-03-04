0a98410577c1fc78ab4334d3e7751b1149b7232e
Alex Campbell <alexbe.campbell@gmail.com>
2021-03-04 17:07:23 -0600

    added screen shot to hard code data for cards

--- src/Pages/PortfolioPage/PortfolioPage.js
+++ src/Pages/PortfolioPage/PortfolioPage.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import {
-  // Card,
-  // CardMedia,
-  // CardActions,
-  // CardContent,
-  // Button,
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Button,
   Grid,
-  // CardHeader,
-  // Avatar,
+  CardHeader,
+  Avatar,
 } from '@material-ui/core';
 
 import './PortfolioPage.css';
 import ProjectListItem from '../../components/ProjectListItem/ProjectListItem';
+const soloScreen = require('./Screen Shot 2021-01-31 at 7.04.02 PM.png');
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -23,29 +24,40 @@ class PortfolioPage extends Component {
   componentDidMount() {
     this.props.dispatch({
       type: 'GET_PROJECT',
+      payload: this.props.store.project,
     });
   }
 
   render() {
-    const projectArray = this.props.store.portfolioReducer.map(
-      (item, index) => {
-        return (
-          <Grid item sm={12} md={4} lg={4}>
-            <ProjectListItem
-              id={item.id}
-              key={index}
-              project={item}
-              index={index}
-            />
-          </Grid>
-        );
-      }
-    );
+    const { project } = this.props;
+    // const projectArray = this.props.store.portfolioReducer.map(
+    //   (item, index) => {
+    //     return (
+    //       <Grid item sm={12} md={4} lg={4}>
+    //         <ProjectListItem
+    //           id={item.id}
+    //           key={index}
+    //           project={item}
+    //           index={index}
+    //         />
+    //       </Grid>
+    //     );
+    //   }
+    // );
     return (
       <div className="container-portfolio">
-        <Grid container spacing={3}>
+        <Card className="card">
+          <CardHeader
+            avatar={<Avatar>C</Avatar>}
+            title="Covid Restaurant Reviews"
+          />
+          <CardContent>
+            <CardMedia image={soloScreen} />
+          </CardContent>
+        </Card>
+        {/* <Grid container spacing={3}>
           {projectArray}
-        </Grid>
+        </Grid> */}
       </div>
     );
   }
